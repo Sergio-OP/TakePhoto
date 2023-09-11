16b7d6a38adff5d72b011d73ed2c37533a87b785
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-11 13:46:47 -0600

    temi face added and action bar hidden

--- app/src/main/java/com/example/takephoto/MainActivity.kt
+++ app/src/main/java/com/example/takephoto/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.takephoto
 
+import android.content.pm.ActivityInfo
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        val videoView =  findViewById<VideoView>(R.id.video_face)
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.temi_face)
+        videoView.setVideoURI(uri)
+        videoView.start()
+        videoView.requestFocus()
+
     }
 }
