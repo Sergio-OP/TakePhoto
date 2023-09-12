0464750d70a2b3322d38b07843344afa2b2a3c8f
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-12 16:15:36 -0600

    temi change the activity when finish to talk

--- app/src/main/java/com/example/takephoto/MainActivity.kt
+++ app/src/main/java/com/example/takephoto/MainActivity.kt
@@ -1,32 +1,51 @@
 package com.example.takephoto
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import com.robotemi.sdk.Robot
 import java.util.Locale
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
 
-    private lateinit var robot: Robot
     private lateinit var textToSpeech: TextToSpeech
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
 
         textToSpeech = TextToSpeech(this, this)
+        textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener(){
+            override fun onStart(p0: String?) {
+                Log.i("TAG", "start speaking")
+            }
+
+            override fun onDone(p0: String?) {
+                Log.i("TAG", "finished speaking")
+                val intent = Intent(this@MainActivity, Question::class.java)
+                startActivity(intent)
+            }
+
+                override fun onError(p0: String?) {
+                Log.i("TAG", "error speaking")
+            }
+
+        })
 
         val videoView =  findViewById<VideoView>(R.id.video_face)
         val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.temi_face)
@@ -37,8 +56,10 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
 
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
-            speakText("Hey! before you go, would you like to take a selfie and capture this great moment?")
-        }, 5000)
+            textToSpeech.speak("Hey! before you go, would you like to take a selfie and capture this great moment?", TextToSpeech.QUEUE_FLUSH, null, "1")
+            //speakText("Hey! before you go, would you like to take a selfie and capture this great moment?")
+        }, 2000)
+
     }
 
     override fun onInit(p0: Int) {
@@ -47,15 +68,13 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
 
             if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED ){
                 Toast.makeText(this, "Language not supported", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Error on initialization of TextToSpeech", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun speakText(text: String) {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
-    }
+    /*private fun speakText(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "1")
+    }*/
 
     override fun onDestroy() {
         super.onDestroy()
@@ -64,4 +83,5 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
             textToSpeech.shutdown()
         }
     }
+
 }
