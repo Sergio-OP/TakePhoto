e5d7e2c0060fda73a086cb54585c14d745c32b85
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-12 11:45:13 -0600

    temi speaks

--- app/src/main/java/com/example/takephoto/MainActivity.kt
+++ app/src/main/java/com/example/takephoto/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.takephoto
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.speech.tts.TextToSpeech
 import android.view.View
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import com.robotemi.sdk.Robot
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
+
+    private lateinit var robot: Robot
+    private lateinit var textToSpeech: TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
+
+        textToSpeech = TextToSpeech(this, this)
 
         val videoView =  findViewById<VideoView>(R.id.video_face)
         val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.temi_face)
@@ -22,7 +35,33 @@ class MainActivity : AppCompatActivity() {
         videoView.requestFocus()
         videoView.setOnPreparedListener { it.setLooping(true) }
 
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            speakText("Hey! before you go, would you like to take a selfie and capture this great moment?")
+        }, 5000)
     }
 
+    override fun onInit(p0: Int) {
+        if(p0 == TextToSpeech.SUCCESS){
+            val result = textToSpeech.setLanguage(Locale.ENGLISH)
 
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED ){
+                Toast.makeText(this, "Language not supported", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error on initialization of TextToSpeech", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun speakText(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(::textToSpeech.isInitialized){
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+    }
 }
